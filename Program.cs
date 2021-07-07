70bb01b4b740b068900e96f9d06329bc43603fd8
tuanhien <icynth0204@gmail.com>
2021-07-07 22:48:52 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleTestApp
+{
+    class Program
+    {
+        public static void CountInputChar(string a) 
+        {
+            ArrayList arrTemp = new ArrayList();
+
+            Console.WriteLine($"Chuoi truoc khi dem co {a.Length} ki tu");
+            var b = a.Replace(" ", "");
+            while(b.Length > 0)
+            {
+                arrTemp.Add(b[0]);
+                for (int j = 1; j < b.Length; j++)
+                {
+                    if (b[0] == b[j])
+                    {
+                        arrTemp.Add(b[j]);
+                        b.Remove(j,1);
+                    }
+                }
+                b = b.Replace($"{b[0]}", "");
+                Console.WriteLine($"Ki tu {arrTemp[0]} co {arrTemp.Count} phan tu");
+                arrTemp.Clear();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string a = "Day la chuoi test";
+            CountInputChar(a);
+        }
+    }
+}
